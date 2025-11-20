0ce41fe43db4befd59eb6debe62d7acb671d69ab
Leilo13 <monroy.j.leonardo@gmail.com>
2025-11-19 19:52:52 -0600

    Versión final definitva, no planeo mover una sola línea

--- src/Accion.java
+++ src/Accion.java
@@ -2,5 +2,4 @@ public enum Accion {
     CHECK("Pasar"), CALL("Igualar"), RAISE("Subir"), ALL_IN("All-In"), FOLD("Retirarse");
     private final String descripcion;
     Accion(String descripcion) { this.descripcion = descripcion; }
-    public String descripcion() { return  descripcion; }
 }
--- src/CategoriaPreflop.java
+++ src/CategoriaPreflop.java
@@ -2,5 +2,4 @@ public enum CategoriaPreflop {
     DELUXE(5), FUERTE(4), DECENTE(3), MALA(2), PESIMA(1);
     private final int peso;
     CategoriaPreflop(int peso) { this.peso = peso; }
-    public int peso() { return peso; }
 }
